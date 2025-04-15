e103e6644c63233ccbebf50cc792e04e04728b5c
Pavan Prasath V <100416549+pavanjack7@users.noreply.github.com>
2025-04-16 00:24:58 +0530

    refactor: variable name change

--- emotion.py
+++ emotion.py
@@ -5,7 +5,7 @@ er=EmotionRecognition(device='cpu')
 cam=cv2.VideoCapture(0)
 
 while True:
-    sucess,frame = cam.read()
+    num,frame = cam.read()
     frame=er.recognise_emotion(frame,return_type='BGR')
     cv2.imshow("Frame",frame)
     key=cv2.waitKey(1)
